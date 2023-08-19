1e992d38fd9616c366327407585ddce60ebee1af
Sean Wong <derpydm@gmail.com>
2023-08-19 17:01:25 +0800

    Initial Commit

---
+++ CHEN/CHENApp.swift
@@ -0,0 +1,17 @@
+//
+//  CHENApp.swift
+//  CHEN
+//
+//  Created by Sean Wong on 19/8/23.
+//
+
+import SwiftUI
+
+@main
+struct CHENApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
