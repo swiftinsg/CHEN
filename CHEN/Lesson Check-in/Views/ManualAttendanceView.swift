c58be07f9e00ff374f46763ac389817679b0b0eb
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2024-03-13 22:17:38 +0800

    fix: fix issue with scanning errors

--- CHEN/Lesson Check-in/Views/ManualAttendanceView.swift
+++ CHEN/Lesson Check-in/Views/ManualAttendanceView.swift
@@ -36,44 +36,44 @@ struct ManualAttendanceView: View {
     @State var search: String = ""
     @State var showAlert = false
     @State var selectedStudent: Student?
+    
     var body: some View {
-        NavigationStack {
-            ZStack {
-                VStack {
-                    // Implement searches properly (see LessonView)
-                    List(searchedStudents) { student in
-                        Button {
-                            selectedStudent = student
-                            showAlert = true
-                        } label: {
-                            Text(student.name ?? "Unknown Data")
-                        }
+        ZStack {
+            VStack {
+                // Implement searches properly (see LessonView)
+                List(searchedStudents) { student in
+                    Button {
+                        selectedStudent = student
+                        showAlert = true
+                    } label: {
+                        Text(student.name ?? "Unknown Data")
                     }
-                    .searchable(text: $search)
                 }
-                .alert(isPresented: $showAlert) {
-                    Alert(title: Text("Attendance"), message: Text("Mark student as attending?"), primaryButton: .default(Text("Yes"), action: {
-                            let attendance = Attendance(context: moc)
-                        attendance.attendanceType = 1
-                        attendance.forLesson = lesson
-                        attendance.person = selectedStudent!
-                        attendance.recordedAt = Date()
+                .searchable(text: $search)
+            }
+            .alert(isPresented: $showAlert) {
+                Alert(title: Text("Attendance"), message: Text("Mark student as attending?"), primaryButton: .default(Text("Yes"), action: {
+                    let attendance = Attendance(context: moc)
+                    attendance.attendanceType = 1
+                    attendance.forLesson = lesson
+                    attendance.person = selectedStudent!
+                    attendance.recordedAt = Date()
+                    
+                    do {
+                        try moc.save()
+                        showAlertToast = true
+                        alertToast = AlertToast(displayMode: .banner(.slide), type: .complete(.green), title: "Success", subTitle: "Attendance marked", style: .style( subTitleFont: .callout))
+                    } catch {
+                        print(error.localizedDescription)
+                        showAlertToast = true
+                        alertToast = AlertToast(displayMode: .banner(.slide), type: .error(.red), title: "An error occured", subTitle: error.localizedDescription)
                         
-                        do {
-                            try moc.save()
-                            showAlertToast = true
-                            alertToast = AlertToast(displayMode: .banner(.slide), type: .complete(.green), title: "Success", subTitle: "Attendance marked", style: .style( subTitleFont: .callout))
-                        } catch {
-                            print(error.localizedDescription)
-                            showAlertToast = true
-                            alertToast = AlertToast(displayMode: .banner(.slide), type: .error(.red), title: "An error occured", subTitle: error.localizedDescription)
-                            
-                        }
-                        
-                        dismiss()
-                        }), secondaryButton: .cancel())
-                }
-            }.navigationTitle(Text("Select Student"))
+                    }
+                    
+                    dismiss()
+                }), secondaryButton: .cancel())
+            }
+            .navigationTitle("Select Student")
         }
         
     }
--- CHEN/Lesson Check-in/Views/ScanView.swift
+++ CHEN/Lesson Check-in/Views/ScanView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 import SwiftNFC
 import CoreData
+import SwiftUINFC
 import CoreNFC
 import AlertToast
 
@@ -23,54 +24,62 @@ struct ScanView: View {
     @State var alertToast: AlertToast = AlertToast(displayMode: .hud, type: .regular, title: "")
     @State var showChangeToast: Bool = false
     
+    @State private var isReaderPresented = false
+    
     var body: some View {
         NavigationStack {
-            VStack {
-                Text(studentName)
-                Button {
-                    reader.read()
-                } label: {
-                    Text("Click here to scan")
-                }
-                NavigationLink() {
-                    ManualAttendanceView(lesson: lesson, alertToast: $alertToast, showAlertToast: $showChangeToast)
-                } label: {
-                    Text("Manually mark a student as attending")
+            List {
+                Section {
+                    Button {
+                        isReaderPresented = true
+                    } label: {
+                        HStack {
+                            Image(systemName: "scanner")
+                                .font(.largeTitle)
+                            VStack(alignment: .leading) {
+                                Text("Scan Badge")
+                                    .font(.headline)
+                                Text("Tap to scan on a student's badge.")
+                            }
+                            .foregroundStyle(Color.primary)
+                            Spacer()
+                        }
+                    }
+                    NavigationLink {
+                        ManualAttendanceView(lesson: lesson, alertToast: $alertToast, showAlertToast: $showChangeToast)
+                    } label: {
+                        Text("Manually mark a student as present")
+                    }
                 }
                 
+                let attendances = lesson.attendances?.array as? [Attendance]
+                
+                Section("Marked attendance") {
+                    ForEach(attendances ?? []) { attendance in
+                        Text(attendance.person?.name ?? "")
+                    }
+                }
             }
-            
-        }
-        .onAppear {
-            reader.startAlert = "Scanning..."
-            reader.completionHandler = { (error) in
-                
-                // Handle error
-                if let unwrappedError = error {
-                    print(unwrappedError.localizedDescription)
-                    print(unwrappedError)
-                    return
+            .nfcReader(isPresented: $isReaderPresented) { messages in
+                guard let message = messages.first,
+                      let record = message.records.first,
+                      let studentUUID = UUID(uuidString: String(decoding: record.payload, as: UTF8.self)) else {
+                    return "This is not a CHEN registered card."
                 }
-                
-                let scannedID = reader.msg
-                if scannedID == "Nothing scanned yet" { return }
-                print("Card read: \(scannedID)")
-                // Find individual
-                
-                // Sanitise the scanned UUID
-                guard let studentUUID = UUID(uuidString: scannedID) else { reader.endAlert = "This isn't a CHEN enrolled card!"; return }
                 
                 let fetchRequest: NSFetchRequest<Student> = Student.fetchRequest()
+                
                 fetchRequest.predicate = NSPredicate(
                     format: "%K == %@", "id", studentUUID as CVarArg
                 )
                 
                 do {
                     let students = try moc.fetch(fetchRequest)
+                    
                     guard let foundStudent = students.first else {
-                        print("Student not found")
-                        return
+                        return "Student not found"
                     }
+                    
                     if let name = foundStudent.name {
                         studentName = name
                         
@@ -82,50 +91,18 @@ struct ScanView: View {
                         
                         attendance.person = foundStudent
                         
-                        reader.endAlert = "Welcome, \(name)"
                         try moc.save()
+                        
+                        return name
+                    } else {
+                        return "Student name not identified"
                     }
                 } catch {
-                    print("There was an error fetching the student: \(error)")
+                    return "There was an error fetching the student: \(error)"
                 }
             }
-            //            .onChange(of: reader.msg) { oldID, scannedID in
-            //                print(oldID)
-            //                if scannedID == "Nothing scanned yet" { return }
-            //                print("Card read: \(scannedID)")
-            //                // Find individual
-            //
-            //                // Sanitise the scanned UUID
-            //                guard let studentUUID = UUID(uuidString: scannedID) else { reader.endAlert = "This isn't a CHEN enrolled card!"; return }
-            //                let fetchRequest: NSFetchRequest<Student> = Student.fetchRequest()
-            //                fetchRequest.predicate = NSPredicate(
-            //                    format: "%K == %@", "id", studentUUID as CVarArg
-            //                )
-            //
-            //                do {
-            //                    let students = try moc.fetch(fetchRequest)
-            //                    guard let foundStudent = students.first else {
-            //                        print("Student not found")
-            //                        return
-            //                    }
-            //                    if let name = foundStudent.name {
-            //                        studentName = name
-            //
-            //                        // create attendance object
-            //                        let attendance = Attendance(context: moc)
-            //                        attendance.attendanceType = 1
-            //                        attendance.forLesson = lesson
-            //                        attendance.recordedAt = Date.now
-            //
-            //                        attendance.person = foundStudent
-            //
-            //                        reader.endAlert = "Welcome, \(name)"
-            //                        try moc.save()
-            //                    }
-            //                } catch {
-            //                    print("There was an error fetching the student: \(error)")
-            //                }
         }
+        .navigationTitle(lesson.session ?? "")
         .toast(isPresenting: $showChangeToast, duration: 1.0) {
             alertToast
         }
